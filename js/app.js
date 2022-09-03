54f589b4786ac5d6e70fd5f053f818bdb359d7fb
Juan Ignacio García <nachogarcia.1017@gmail.com>
2022-09-03 10:51:47 -0300

    contacto.html funcional

--- js/app.js
+++ js/app.js
@@ -11,5 +11,38 @@ btnSwitch.addEventListener('click', (e) => {
 })
 
 // Obtenemos el modo actual.
-
 localStorage.getItem('dark-mode') === 'true' ? document.body.classList.add('dark') : document.body.classList.remove('dark')
+
+
+
+
+// Notificaciones para contactos.html
+const enviarFormulario = document.getElementById('enviarForm')
+const resetFormulario = document.getElementById('resetForm')
+
+enviarFormulario.onclick = (e) => {
+    e.preventDefault
+    Toastify({
+        text: "Enviando formulario, por favor, espere hasta ser redirigido.",
+        avatar: "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/whatsapp/238/white-heavy-check-mark_2705.png",
+        duration: 5000,
+        stopOnFocus: false,
+        className: "info",
+        style: {
+            background: "linear-gradient(to right, #15B206, #51E721)",
+        }
+    }).showToast();
+}
+
+resetFormulario.onclick = () => {
+    Toastify({
+        text: "Formulario restablecido exitósamente.",
+        avatar: "https://cdn-0.emojis.wiki/emoji-pics/facebook/cross-mark-button-facebook.png",
+        duration: 2000,
+        stopOnFocus: false,
+        className: "info",
+        style: {
+            background: "linear-gradient(to right, #FE8227, #FE2727)",
+        }
+    }).showToast();
+}
